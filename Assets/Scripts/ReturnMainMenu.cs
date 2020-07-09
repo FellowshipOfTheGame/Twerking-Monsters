e2fddab77f5373c0434cc6d0f92e672cb442dacc
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-07-09 14:51:09 -0300

    Pequenos Ajustes

---
+++ Assets/Scripts/ReturnMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMainMenu : MonoBehaviour
+{
+    public float timeToMainMenu = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("MainMenu");
+    }
+
+    public IEnumerator MainMenu()
+    {
+        yield return new WaitForSeconds(timeToMainMenu);
+          SceneManager.LoadScene("Menu");
+    }
+}
